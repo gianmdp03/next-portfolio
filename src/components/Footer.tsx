6cfbb50a7b8680a74b6110c1a56062e0e9218c76
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2026-06-24 17:42:09 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Footer = ({ githubLink, linkedinLink }: Props) => {
   return (
-    <footer className="max-w-3xl mx-auto px-6 mt-28 border-t border-zinc-900 pt-8 pb-12 flex flex-col md:flex-row items-center justify-between gap-4 font-mono text-xs text-zinc-600">
+    <footer className="max-w-3xl mx-auto px-6 mt-16 border-t border-zinc-900 pt-8 pb-12 flex flex-col md:flex-row items-center justify-between gap-4 font-mono text-xs text-zinc-600">
       <p>// © 2026 Gianluca Castorina.</p>
       <div className="flex gap-4">
         <Link
